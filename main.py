a4cc67b45da7ed91e581fd54f9d64bf3e4934433
HyperLexus <alexberg1444@gmail.com>
2024-04-09 16:17:43 +0200

    working on it

--- main.py
+++ main.py
@@ -1,64 +1,69 @@
 import random
+import math
 
 final_odds = 0
 artifacts = ["flower", "feather", "sands", "goblet", "circlet"]
 # main stat odds
-flower_odds = {"HP": 100}
-feather_odds = {"ATK": 100}
-# all values except EM are in %
-sands_odds = {"HP": 26.68, "ATK": 26.66, "DEF": 26.66, "ER": 10, "EM": 10}
-goblet_odds = {"HP": 19.25, "ATK": 19.25, "DEF": 19, "pDMG": 5, "eDMG": 5, "cDMG": 5, "hDMG": 5, "dDMG": 5, "aDMG": 5, "gDMG": 5, "physDMG": 5, "EM": 2.5}
-circlet_odds = {"HP": 22, "ATK": 22, "DEF": 22, "CR": 10, "CD": 10, "HB": 10, "EM": 4}
-
-
+mainstat_odds = [
+{"HP": 1.0},
+{"ATK": 1.0},
+{"HP": 0.2668, "ATK": 0.2666, "DEF": 0.2666, "ER": 0.1, "EM": 0.1},
+{"HP": 0.1925, "ATK": 0.1925, "DEF": 0.19, "pDMG": 0.05, "eDMG": 0.05, "cDMG": 0.05, "hDMG": 0.05, "dDMG": 0.05, "aDMG": 0.05, "gDMG": 0.05, "physDMG": 0.05, "EM": 0.025},
+{"HP": 0.22, "ATK": 0.22, "DEF": 0.22, "CR": 0.1, "CD": 0.1, "HB": 0.1, "EM": 0.04}
+]
 
 # data from the wiki
 double_5star = 0.061032864
 domain_four_stats_odds = 0.2
-strongbox_four_stats_odds = 0.34
-set_odds = 0.5
-correct_artifact_odds = 0.2
+strongbox_four_stats_odds = 0.34 #yes, not rounded
 
 modes = [
-"generate one random artifact",
+"simulate X domain runs",
 "generate X random artifacts",
 "generate artifacts until a certain correct artifact comes out",
-"generate artifacts until the correct artifact with all correct substats comes out"
+"generate artifacts until the correct artifact with all correct substats comes out",
+"generate artifacts until the correct artifact with all correct substats and all correct rolls comes out"
 ]
-def generate_empty_artifact():
+
+def generate_mainstat_artifact():
     odds = []
     results = []
-    for j in range(5):
+    for j in range(8):
         odds.append(random.random())
-    if double_5star < odds[0]:
-        results.append(1)
-        double = True
-    else:
-        results.append(0)
-    if double == True:
-        artifact2 = []
+
+    #on-set or off-set
+    results.append(round(odds[0]))
+    #what piece
+    results.append(math.floor(odds[1]*5)) if odds[1]<0.99999 else results.append(4)
+    # main stat
 
 
-    return odds, results
+    print(odds)
+    return results
 
-mode = input("mode: ")
+mode = input("Enter mode: ")
 mode_done = False
 while not mode_done:
     try:
         mode = int(mode)
     except:
         print("please input a number. try again.")
-        mode = input("mode: ")
+        mode = input("Enter mode: ")
         continue
 
-    if -1<mode<4:
+    if -1<mode<5:
         mode_done = True
     else:
-        print("enter either 0,1,2,3. try again.")
-        mode = input("mode: ")
+        print("enter only integers from (including) 0-4. try again.")
+        mode = input("Enter mode: ")
 
 print(f"Mode {mode}: " + modes[mode])
-print(generate_empty_artifact())
+print("artifact:" ,generate_mainstat_artifact())
+
+
+# results:
+# on-set, what piece, main stat, sub-stat 1, sub-stat 2, sub-stat 3, sub-stat 4 (if applicable)
+
 
 # match mode:
 #     case 0:
---
+++ test.py
@@ -0,0 +1,12 @@
+import random
+
+results_odds = {"HP": 19.25, "ATK": 19.25, "DEF": 19, "pDMG": 5, "eDMG": 5, "cDMG": 5, "hDMG": 5, "dDMG": 5, "aDMG": 5, "gDMG": 5, "physDMG": 5, "EM": 2.5}
+
+random_pick = random.random() * 100
+
+current_odds = 0
+for result, odds in results_odds.items():
+    current_odds += odds
+    if current_odds >= random_pick:
+        print("Selected result:", result, ", number:", random_pick/100)
+        break
