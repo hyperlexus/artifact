be6469f7fae8a38832a8ec4591b9327f06a5726a
HyperLexus <alexberg1444@gmail.com>
2024-04-10 17:18:23 +0200

    substat thing working on it but its late so im stopping lmao

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 import random
 import math
+import sys
 
-final_odds = 0
-artifacts = ["flower", "feather", "sands", "goblet", "circlet"]
-# main stat odds
+# data from the wiki
+artifact_list = ["flower", "feather", "sands", "goblet", "circlet"]
 mainstat_odds = [
 {"HP": 1.0},
 {"ATK": 1.0},
-{"HP": 0.2668, "ATK": 0.2666, "DEF": 0.2666, "ER": 0.1, "EM": 0.1},
-{"HP": 0.1925, "ATK": 0.1925, "DEF": 0.19, "pDMG": 0.05, "eDMG": 0.05, "cDMG": 0.05, "hDMG": 0.05, "dDMG": 0.05, "aDMG": 0.05, "gDMG": 0.05, "physDMG": 0.05, "EM": 0.025},
-{"HP": 0.22, "ATK": 0.22, "DEF": 0.22, "CR": 0.1, "CD": 0.1, "HB": 0.1, "EM": 0.04}
+{"%HP": 0.2668, "%ATK": 0.2666, "%DEF": 0.2666, "ER": 0.1, "EM": 0.1},
+{"%HP": 0.1925, "%ATK": 0.1925, "%DEF": 0.19, "pDMG": 0.05, "eDMG": 0.05, "cDMG": 0.05, "hDMG": 0.05, "dDMG": 0.05, "aDMG": 0.05, "gDMG": 0.05, "physDMG": 0.05, "EM": 0.025},
+{"%HP": 0.22, "%ATK": 0.22, "%DEF": 0.22, "CR": 0.1, "CD": 0.1, "HB": 0.1, "EM": 0.04}
 ]
-
-# data from the wiki
+substat_weight = {"HP": 6, "ATK": 6, "DEF": 6, "%HP": 4, "%ATK": 4, "%DEF": 4, "ER": 4, "EM": 4, "CR": 4, "CD": 4}
+#other stuff
 double_5star = 0.061032864
-domain_four_stats_odds = 0.2
-strongbox_four_stats_odds = 0.34 #yes, not rounded
 
+# possible running modes
 modes = [
-"simulate X domain runs",
 "generate X random artifacts",
+"simulate X domain runs",
 "generate artifacts until a certain correct artifact comes out",
 "generate artifacts until the correct artifact with all correct substats comes out",
 "generate artifacts until the correct artifact with all correct substats and all correct rolls comes out"
 ]
 
-def generate_mainstat_artifact():
+def new_mainstat_artifact():
     odds = []
     results = []
-    for j in range(8):
+    for j in range(3):
         odds.append(random.random())
 
     #on-set or off-set
@@ -36,19 +35,43 @@ def generate_mainstat_artifact():
     print("on-set") if odds[0] > 0.5 else print("off-set")
     #what piece
     results.append(math.floor(odds[1]*5)) if odds[1]<0.99999 else results.append(4)
-    print(artifacts[results[1]])
+    print(artifact_list[results[1]])
     # main stat
-    current_odds = 0
-    curr_index = 0
+    matcher = 0
+    index = 0
     for stat, chance in mainstat_odds[results[1]].items():
-        current_odds += chance
-        if current_odds >= odds[2]:
+        matcher += chance
+        if matcher >= odds[2]:
             print(stat)
-            results.append(curr_index)
+            results.append(index)
             break
-        curr_index += 1
+        index += 1
+    return results
 
-    print(odds)
+def add_substats(artifact, mode):
+    odds = []
+    results = []
+    four_stats = False
+    for j in range(4):
+        odds.append(random.random())
+
+    #3 or 4 substats, domain = 0 and strongbox = 1
+    if mode == 0:
+        if odds[0] < 0.2:
+            four_stats = True
+    elif mode == 1:
+        if odds[0] < 0.34:
+            four_stats = True
+    else:
+        print("im a idiot")
+        sys.exit(69420)
+
+    sum_substats = 46
+    mod_dict = substat_weight
+    mod_dict.pop("%HP")
+    print(mainstat_odds[2].items())
+    print(mod_dict)
+
     return results
 
 mode = input("Enter mode: ")
@@ -68,11 +91,12 @@ while not mode_done:
         mode = input("Enter mode: ")
 
 print(f"Mode {mode}: {modes[mode]}")
-print("artifact:" ,generate_mainstat_artifact())
 
+artifact = new_mainstat_artifact()
+sub_artifact = add_substats(artifact, int(input('input substat mode: ')))
+
+print("artifact:", artifact)
+print("substats:", sub_artifact)
 
 # results:
 # on-set, what piece, main stat, sub-stat 1, sub-stat 2, sub-stat 3, sub-stat 4 (if applicable)
-
-
-
--- test.py
+++ test.py
@@ -10,3 +10,17 @@ for result, odds in results_odds.items():
     if current_odds >= random_pick:
         print("Selected result:", result, ", number:", random_pick/100)
         break
+
+
+print(results_odds.items())
+
+# discord fuckery
+handshakes = ""
+for i in range(1, 6):
+    for j in range(1, 6):
+        if i != j:
+            handshakes += ":handshake_tone" + str(j) + "_tone" + str(i) + ": "
+        else:
+            continue
+print(handshakes)
+
